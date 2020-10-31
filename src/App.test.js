8013c473a03199fc46e5792d38483b0cce7ffb9c
tutkowskim <tutkows1@gmail.com>
2020-10-30 21:04:50 -0500

    Get tests in place

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const { getByText } = render(<BrowserRouter><App /></BrowserRouter>);
+  const linkElement = getByText(/Hello, my name is Mark Tutkowski./i);
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/ProjectData.test.js
@@ -0,0 +1,25 @@
+import ProjectData from './ProjectData.json';
+
+const requiredFields = ['name', 'description', 'coverImageUrl', 'projectUrl'];
+const optionalFields = ['productionUrl'];
+const validFields = [...requiredFields, ...optionalFields];
+
+test('verify the project data is a non-empty array', () => {
+  expect(Array.isArray(ProjectData)).toBe(true);
+  expect(ProjectData.length > 0).toBe(true);
+});
+
+ProjectData.forEach(project => {
+  test(`check all fields are valid in ${JSON.stringify(project)}`, () => {
+    Object.keys(project).forEach(field => {
+      expect(validFields.includes(field)).toBeTruthy();
+    });
+  });
+
+  test(`check required fields exist in ${JSON.stringify(project)}`, () => {
+    requiredFields.forEach(field => {
+      expect(project[field]).toBeTruthy();
+      expect(typeof project[field] == 'string').toBeTruthy();
+    });
+  });
+});
