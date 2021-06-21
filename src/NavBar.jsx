879bc294d378db3921064fad30d847526c631e06
Mark Tutkowski <mark@tutkowski.com>
2021-06-21 17:29:59 -0500

    Use breakpoint built into library.

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -13,11 +13,11 @@ import {
   ListItem,
   ListItemText,
 } from '@material-ui/core';
+import withWidth, { isWidthUp } from '@material-ui/core/withWidth';
 
 import MenuIcon from '@material-ui/icons/Menu';
 
 import { routeInfo } from './routes';
-import useWindowSize from './useWindowSize';
 
 const useStyles = makeStyles((theme) => ({
   toolBar: {
@@ -34,12 +34,11 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function NavBar() {
+function NavBar(props) {
   const visibleRoutes = routeInfo.filter((routeInfo) => routeInfo.showInNavMenu);
 
   const classes = useStyles();
   const history = useHistory();
-  const windowSize = useWindowSize();
 
   const [showDrawer, setShowDrawer] = React.useState(false);
   const changePage = (route) => {
@@ -51,7 +50,7 @@ function NavBar() {
     <AppBar position="static">
       <Toolbar className={classes.toolBar}>
         <Typography variant="h6" className={classes.title}>Mark Tutkowski</Typography>
-        {windowSize.width > 600 ? (
+        {isWidthUp('sm', props.width) ? (
           <>
             { visibleRoutes.map((route) => <Button key={route.name} className={classes.menuButton} color="inherit" onClick={() => changePage(route.path)}>{route.name}</Button>) }
           </>
@@ -76,4 +75,4 @@ function NavBar() {
   );
 }
 
-export default NavBar;
+export default withWidth()(NavBar);
