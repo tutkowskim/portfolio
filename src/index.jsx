dc1cfe0c7c6e5b00500d27daab529f8c3faf1b17
Mark Tutkowski <mark@tutkowski.com>
2021-06-23 08:51:46 -0500

    Have the react router update the google analtics pageview.

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 
 import './index.css';
 import App from './App';
 
+const history = createBrowserHistory();
+
+history.listen((location) => {
+  window.ga('set', 'page', location.pathname + location.search);
+  window.ga('send', 'pageview');
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter history={history} >
       <App />
     </BrowserRouter>
   </React.StrictMode>,
