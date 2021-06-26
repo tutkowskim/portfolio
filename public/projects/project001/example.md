c0a7d4e2611a59ad3dc21aefb63e1c1ec1760f0c
Mark Tutkowski <mark@tutkowski.com>
2021-06-26 14:28:51 -0500

    Support writing the project pages in markdown.

--- public/projects/project001/example.md
+++ public/projects/project001/example.md
@@ -1,1 +1,2 @@
+#### Example
 More coming soon!
--- src/routes/components/Project.jsx
+++ src/routes/components/Project.jsx
@@ -1,28 +1,44 @@
 import React from 'react';
-import { useParams } from "react-router-dom";
-import { Typography, makeStyles } from '@material-ui/core';
+import { useParams } from 'react-router-dom';
+import ReactMarkdown from 'react-markdown';
+import { Typography, Link, makeStyles } from '@material-ui/core';
 import useProjectMarkdown from './useProjectMarkdown';
 
+/* eslint-disable react/display-name */
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-props-no-spreading */
+
 const useStyles = makeStyles(() => ({
-  container: {
+  markdown: {
     height: '100%',
     display: 'flex',
     flexDirection: 'column',
-    justifyContent: 'center',
-    textAlign: 'center',
-    gap: '1rem',
+    paddingTop: '2rem',
   },
 }));
 
-function Connect() {
+function Project() {
   const classes = useStyles();
   const { projectName } = useParams();
-  const markdown = useProjectMarkdown(projectName)
+  const markdown = useProjectMarkdown(projectName);
+
+  // Map the markdown components to material-ui components
+  // https://github.com/remarkjs/react-markdown#appendix-b-components
+  const components = {
+    h1: ({ node, children, ...props }) => <Typography variant="h1" {...props}>{children}</Typography>,
+    h2: ({ node, children, ...props }) => <Typography variant="h2" {...props}>{children}</Typography>,
+    h3: ({ node, children, ...props }) => <Typography variant="h3" {...props}>{children}</Typography>,
+    h4: ({ node, children, ...props }) => <Typography variant="h4" {...props}>{children}</Typography>,
+    h5: ({ node, children, ...props }) => <Typography variant="h5" {...props}>{children}</Typography>,
+    h6: ({ node, children, ...props }) => <Typography variant="h6" {...props}>{children}</Typography>,
+    p: ({ node, children, ...props }) => <Typography variant="body1" {...props}>{children}</Typography>,
+    em: ({ node, children, ...props }) => <Typography variant="body1" {...props}>{children}</Typography>,
+    a: ({ node, children, ...props }) => <Link {...props}>{children}</Link>,
+  };
+
   return (
-    <div className={classes.container}>
-      <Typography>{markdown}</Typography>
-    </div>
+    <ReactMarkdown className={classes.markdown} components={components}>{markdown}</ReactMarkdown>
   );
 }
 
-export default Connect;
+export default Project;
