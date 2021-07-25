1b4be4df1c91b475a4d411aa12c80071398f826c
Mark Tutkowski <mark@tutkowski.com>
2021-07-24 21:45:06 -0500

    Update react-scripts and eslint

--- src/routes/components/index.js
+++ src/routes/components/index.js
@@ -2,4 +2,3 @@ export { default as About } from './About';
 export { default as Connect } from './Connect';
 export { default as Work } from './Work';
 export { default as Project } from './Project';
- 
