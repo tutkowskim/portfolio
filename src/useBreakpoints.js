5d4fbf3fe9c8061411c891ff3519c57953bd8b65
Mark Tutkowski <mark@tutkowski.com>
2022-07-23 09:18:26 -0500

    Update breakpoint sizes

--- src/useBreakpoints.js
+++ src/useBreakpoints.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 export const BreakPoints = { SMALL: 'x', MEDIUM: 'm', LARGE: 'l' };
 
 const calculateBreakpoint = () => {
-  if (window.innerWidth < 720) return BreakPoints.SMALL;
+  if (window.innerWidth < 750) return BreakPoints.SMALL;
   if (window.innerWidth < 1100) return BreakPoints.MEDIUM;
   return BreakPoints.LARGE;
 };
