34212176910e5b471a6229476c34b8122d908335
Mark Tutkowski <mark@tutkowski.com>
2023-02-25 22:19:46 -0600

    Update projects list

--- src/ProjectsSection.jsx
+++ src/ProjectsSection.jsx
@@ -5,6 +5,19 @@ const sectionIntro = 'I\'ve found that one of the best ways to learn new things 
 
 const projects = [
   {
+    name: 'News Word Cloud',
+    imageUrl: '/project-logos/news-word-cloud.png',
+    description: 'A news search bar which creates a word cloud to highlight popular words in recent articles to help visualize popular topics.',
+    technologiesUsed: [
+      { name: 'Typescript', icon: '/technology-icons/typescript.svg' },
+      { name: 'Angular', icon: '/technology-icons/angular.svg' },
+      { name: 'HTML', icon: '/technology-icons/html.svg' },
+      { name: 'CSS', icon: '/technology-icons/css.svg' },
+    ],
+    githubUrl: 'https://github.com/tutkowskim/news-word-cloud',
+    projectUrl: 'https://www.news.tutkowski.com',
+  },
+  {
     name: 'Wine not Splurge?',
     imageUrl: '/project-logos/wine-not-splurge.png',
     description: 'Wine not Splurge is a visual narration I created to explore the D3 library. The narration explores the correlation between the cost of wine and it\'s quality and attempts to answer the question if it\'s worth spending a little extra on a bottle of wine.',
@@ -13,55 +26,8 @@ const projects = [
       { name: 'HTML', icon: '/technology-icons/html.svg' },
       { name: 'CSS', icon: '/technology-icons/css.svg' },
     ],
-    githubUrl: 'https://github.com/tutkowskim/nerf-blaster',
+    githubUrl: 'https://github.com/tutkowskim/wine-not-splurge',
     projectUrl: 'https://www.wine-not-splurge.tutkowski.com',
-  },
-  {
-    name: 'Nerf Blaster',
-    imageUrl: '/project-logos/nerf-blaster.jpeg',
-    description: 'Nerf gun battles are not only a fun toy for kids, but can also be fun in office environments used for team building and comradery! This project remotely controls a nerf blaster using two servos to aim the device, a linear actuator to fire the device, and the pi-camera and tensorflow libraries to perform image tracking.',
-    technologiesUsed: [
-      { name: 'Angular', icon: '/technology-icons/angular.svg' },
-      { name: 'Python', icon: '/technology-icons/python.svg' },
-      { name: 'HTML', icon: '/technology-icons/html.svg' },
-      { name: 'CSS', icon: '/technology-icons/css.svg' },
-    ],
-    githubUrl: 'https://github.com/tutkowskim/nerf-blaster',
-    projectUrl: undefined,
-  },
-  {
-    name: 'Task Tracker',
-    imageUrl: '/project-logos/task-tracker.png',
-    description: 'Task tracker is a react application, which I created in order to explore React and Azure Serverless Functions.',
-    technologiesUsed: [
-      { name: 'React', icon: '/technology-icons/react.svg' },
-      { name: 'Javascript', icon: '/technology-icons/javascript.svg' },
-      { name: 'Typescript', icon: '/technology-icons/typescript.svg' },
-      { name: 'HTML', icon: '/technology-icons/html.svg' },
-      { name: 'CSS', icon: '/technology-icons/css.svg' },
-    ],
-    githubUrl: 'https://github.com/tutkowskim/TaskTracker',
-    projectUrl: undefined,
-  },
-  {
-    name: 'Scrum Poker',
-    imageUrl: '/project-logos/scrum-poker.png',
-    description: 'This project is a light weight scrum poker planning application, which I was motivated to create after the scrum poker app that I had been using started to slow down with unnecessary animations and ads. The application consists of a deck view and the card view, which allow the user to select a planning card from the Planning Poker cards designed by Redbooth and are licensed under Creative Commons – Attribution (CC BY 3.0).',
-    technologiesUsed: [
-      { name: 'Java', icon: '/technology-icons/java.svg' },
-    ],
-    githubUrl: 'https://github.com/tutkowskim/ScrumPoker',
-    projectUrl: 'https://play.google.com/store/apps/details?id=poker.scrum.tutkowski.scrumpoker',
-  },
-  {
-    name: 'Cowsay',
-    imageUrl: '/project-logos/cowsay.png',
-    description: 'This project is a port for the fun linux cowsay application to Android, which allows the user to share the message in raw text format or as an image using intents.',
-    technologiesUsed: [
-      { name: 'Java', icon: '/technology-icons/java.svg' },
-    ],
-    githubUrl: 'https://github.com/tutkowskim/AndroidCowsay',
-    projectUrl: 'https://play.google.com/store/apps/details?id=cowsay.andriod.tutkowskim.androidcowsay',
   },
 ];
 
